ca91540af707fb0b709033a7a5b4d32a9bfaf48b
Srikanth <v.srikanth@mokriya.com>
2017-03-26 11:31:36 +0530

    Updated Students Information

--- app/src/main/java/com/android/meetupdemo/MainActivity.java
+++ app/src/main/java/com/android/meetupdemo/MainActivity.java
@@ -2,12 +2,16 @@ package com.android.meetupdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvStudentsInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvStudentsInfo=(RecyclerView)findViewById(R.id.rv_students_info);
     }
 }
